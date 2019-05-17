56f69f8571e5e62e44b39a5497c0deabd2cdaca6
nkumnick <nicolaskumnick@bitbucket.org>
2019-05-17 23:11:46 +0200

    complete redo of result

--- src/js/components/result/Result.js
+++ src/js/components/result/Result.js
@@ -1,51 +1,95 @@
-import React from 'react'
-import {Step, Segment, Grid} from 'semantic-ui-react'
+import React, {Component, Fragment} from 'react'
+import {Divider, Table, Header, Icon, Segment, Grid} from 'semantic-ui-react'
 
 
-const Results = () => (
+export default class Results extends Component {
+    fetchResults = [
+        {
+            0: {
+                transport: 'Flight',
+                emission: '10kg',
+                distance: '1000km',
+                description: 'Fancy things about plane flights are bad!',
+                icon: 'tba'
+            }
+        }, {
+            1: {
+                transport: 'Car',
+                emission: '8kg',
+                distance: '600km',
+                description: 'Fancy things about car rides are bad!',
+                icon: 'tba'
+            }
+        }, {
+            2: {
+                transport: 'Train',
+                emission: '4kg',
+                distance: '300km',
+                description: 'Fancy things about train rides are bad!',
+                icon: 'tba'
+            }
+        }
+    ];
 
-    <div>
-        <Segment>
-            <Grid columns={4} divided>
-                <Grid.Row>
-                    <Grid.Column>
+    descriptionResults = ['Transport', 'Emission Value', 'Distance']
 
-                        <Step.Group>
-                            <Step>
-                                <Step.Title>RESULTS</Step.Title>
-                                <Step.Description>Choose your </Step.Description>
-                            </Step>
-                        </Step.Group>
-                    </Grid.Column>
-                    <Grid.Column>
-                        <Step.Group>
-                            <Step>
-                                <Step.Title>RESULTS</Step.Title>
-                                <Step.Description>Choose your </Step.Description>
-                            </Step>
-                        </Step.Group>
-                    </Grid.Column>
-                    <Grid.Column>
-                        <Step.Group>
-                            <Step>
-                                <Step.Title>RESULTS</Step.Title>
-                                <Step.Description>Choose your </Step.Description>
-                            </Step>
-                        </Step.Group>
-                    </Grid.Column>
-                    <Grid.Column>
-                        <Step.Group>
-                            <Step>
-                                <Step.Title>RESULTS</Step.Title>
-                                <Step.Description>Choose your </Step.Description>
-                            </Step>
-                        </Step.Group>
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
-        </Segment>
-    </div>
+    _renderFetchResults() {
+        return Object.entries(this.fetchResults).map(([key, value], i) => {
+            return (
+                <Fragment key={key}>
+                    <Divider horizontal>
+                        <Header as='h4'>
+                            <Icon name='tag'/>
+                        </Header>
+                    </Divider>
+                    <p>
+                        {value[key].description}
+                    </p>
 
-);
+                    <Divider horizontal>
+                        <Header as='h4'>
+                            <Icon name='bar chart'/>
+                            Details
+                        </Header>
+                    </Divider>
 
-export default Results
+                    <Table definition>
+                        <Table.Body>
+                            <Grid columns={4} divided>
+                                <Grid.Row>
+                                    <Grid.Column>
+                                        <Table.Row>
+                                            <Table.Cell width={2}>Transport</Table.Cell>
+                                            <Table.Cell>{value[key].transport}</Table.Cell>
+                                        </Table.Row>
+                                        <Table.Row>
+                                            <Table.Cell width={2}>Emission Value</Table.Cell>
+                                            <Table.Cell>{value[key].emission}</Table.Cell>
+                                        </Table.Row>
+                                        <Table.Row>
+                                            <Table.Cell width={2}>Distance</Table.Cell>
+                                            <Table.Cell>{value[key].distance}</Table.Cell>
+                                        </Table.Row>
+                                    </Grid.Column>
+                                </Grid.Row>
+                            </Grid>
+                        </Table.Body>
+                    </Table>
+                </Fragment>
+            )
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <Segment>
+                    <Header as='h4' textAlign='center'>Results</Header>
+                    {this._renderFetchResults()}
+                </Segment>
+            </div>
+
+        )
+    };
+}
+
