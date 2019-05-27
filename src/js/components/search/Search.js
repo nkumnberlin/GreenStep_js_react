67385abeb8408b8f67b88eb948e9cd3181651b4e
nkumnick <nicolaskumnick@bitbucket.org>
2019-05-27 14:54:04 +0200

    hass
    i hate my life, restart #1

--- src/js/components/search/Search.js
+++ src/js/components/search/Search.js
@@ -1,25 +1,37 @@
 import React, {Component} from 'react';
 
+import Script from 'react-load-script';
 import {Segment, Grid, Form, Button} from 'semantic-ui-react'
 import Header from "semantic-ui-react/dist/commonjs/elements/Header";
 import Departure from './departure.js'
-import Arrival from './arrival.js'
+import Divider from "semantic-ui-react/dist/commonjs/elements/Divider";
 import postCords from "../python_endpoint/PythonPost";
+<<<<<<< HEAD
 import GooglePlacesSuggest from "react-google-places-suggest"
 import GoogleMapLoader from "react-google-maps-loader"
 
 const MY_API_KEY = "AIzaSyDo6leoat6ziQnl9n6oIsgYwSz5BopUfPM";
+=======
+import Result from "../result/Result";
+>>>>>>> parent of 7449b079... prep for code testing
 
 
-let data_export = "";
 
+const form_style = {
+    margin: '0 auto',
+    maxWidth: 800,
+};
+
+export let data_export = "";
 
 export default class Search extends Component {
     // Define Constructor
     constructor(props) {
         super(props);
 
+        // // Declare State
         this.state = {
+<<<<<<< HEAD
             depSearch: "",
             depValue: "",
             arrSearch: "",
@@ -53,14 +65,111 @@ export default class Search extends Component {
             console.log('No results for ', this.state.depSearch)
         }else{
             console.log('No results for ', this.state.arrSearch)
+=======
+            lat: '',
+            lng: ''
+        };
+        // Bind Functions
+        this.handleScriptLoad = this.handleScriptLoad.bind(this);
+        this.handleDeparture = this.handleDeparture.bind(this);
+        this.handleArrival = this.handleArrival.bind(this);
+
+    }
+
+    fields = ['address_components', 'geometry', 'icon', 'name'];
+    initialize = () => {
+        this.geocoder = new google.maps.Geocoder();
+    };
+
+    handleScriptLoad = () => {
+        this.initialize();
+        this.departure = new google.maps.places.Autocomplete(
+            document.getElementById('departure'));
+        this.arrival = new google.maps.places.Autocomplete(
+            document.getElementById('arrival'));
+
+        this.departure.setFields(this.fields);
+        this.departure.addListener('place_changed', this.handleDeparture);
+        this.arrival.setFields(this.fields);
+        this.arrival.addListener('place_changed', this.handleArrival);
+    };
+
+    handleDeparture = () => {
+        let place = this.departure.getPlace();
+        if (!place.geometry) {
+            // User entered the name of a Place that was not suggested and
+            // pressed the Enter key, or the Place Details request failed.
+            console.log("No details available for input: '" + place.name + "'");
+            return;
+        }
+        let address = place.address_components;
+        let lat = place.geometry.location.lat();
+        let lng = place.geometry.location.lng();
+        // Check if address is valid
+        console.log(address);
+        if (address) {
+            // Set State
+            this.setState(
+                {
+                    lat: lat,
+                    lng: lng
+                }
+            );
+            this.codeLatLng(lat, lng);
+            console.log(this.state)
         }
     };
 
-    handleStatusUpdate = (status) => {
-        console.log(status)
+    handleArrival = () => {
+        let place = this.arrival.getPlace();
+        if (!place.geometry) {
+            // User entered the name of a Place that was not suggested and
+            // pressed the Enter key, or the Place Details request failed.
+            console.log("No details available for input: '" + place.name + "'");
+            return;
+        }
+        let address = place.address_components;
+        let lat = place.geometry.location.lat();
+        let lng = place.geometry.location.lng();
+        // Check if address is valid
+        console.log(address);
+        if (address) {
+            // Set State
+            this.setState(
+                {
+                    lat: lat,
+                    lng: lng
+                }
+            );
+            this.codeLatLng(lat, lng);
+            console.log(this.state)
+>>>>>>> parent of 7449b079... prep for code testing
+        }
     };
 
+
+    codeLatLng = (lat, lng) => {
+        let latlng = new google.maps.LatLng(lat, lng);
+        this.geocoder.geocode({
+            'latLng': latlng
+        }, function (results, status) {
+            if (status === google.maps.GeocoderStatus.OK) {
+                if (results[1]) {
+                    console.log(results[1]);
+                } else {
+                    alert('No results found');
+                }
+            } else {
+                alert('Geocoder failed due to: ' + status);
+            }
+        });
+
+        // PythonPost.setState()
+    };
+
+
     render() {
+<<<<<<< HEAD
         const {depSearch, arrSearch} = this.state;
         return (
             <GoogleMapLoader
@@ -101,73 +210,36 @@ export default class Search extends Component {
                 }
             />
         )
+=======
+        return (
+            <div>
+                <Script
+                    url="https://maps.googleapis.com/maps/api/js?key=AIzaSyDo6leoat6ziQnl9n6oIsgYwSz5BopUfPM&libraries=places"
+                    onLoad={this.handleScriptLoad}
+                />
+                <Segment padded className={'search'}>
+                    <Header textAlign={'center'} as='h3'> Plan your Trip and Compensate your Emission </Header>
+                    <Grid>
+                        <Grid.Row>
+                            <Grid.Column width={12}>
+                                <Form>
+                                    <Form.Input id={'departure'} placeholder={'Departure'}
+                                                style={form_style}
+                                    />
+                                </Form>
+                                <Form>
+                                    <Form.Input id={'arrival'} placeholder={'Arrival'}
+                                                style={form_style}/>
+                                </Form>
+                            </Grid.Column>
+                            <Grid.Column verticalAlign={'middle'} textAlign={'center'} width={4}>
+                                <Button onClick={ data_export = postCords} positive> Submit </Button>
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
+                </Segment>
+            </div>
+        );
+>>>>>>> parent of 7449b079... prep for code testing
     }
-
-
-    //
-    // handleScriptLoad(){
-    // console.log(this.props.departure);
-    //     this.rawPlace = new google.maps.places.Autocomplete(
-    //         document.getElementById('departure')
-    //     );
-    //     // this.rawPlace.setFields(this.fields);
-    //     // this.rawPlace.addListener('place_changed', this.handleCords(element));
-    // }
-    //
-    // handleCords(element){
-    //     let place = this.rawPlace.getPlace();
-    //         if (!place.geometry) {
-    //             // User entered the name of a Place that was not suggested and
-    //             // pressed the Enter key, or the Place Details request failed.
-    //             console.log("No details available for input: '" + place.name + "'");
-    //             return;
-    //         }
-    //         this.lat = place.geometry.location.lat();
-    //         this.lng = place.geometry.location.lng();
-    //         this.saveCoordinates(element)
-    // }
-    //
-    // saveCoordinates(element){
-    //     if(element.toString() === 'departure') {
-    //         this.setState({
-    //             Departure: {
-    //                 lat: this.lat,
-    //                 lng: this.lng
-    //             }
-    //         })
-    //     }else{
-    //         this.setState({
-    //             Arrival: {
-    //                 lat: this.lat,
-    //                 lng: this.lng
-    //             }
-    //         })
-    //     }
-    // }
-    //
-
-
-    //
-    // render() {
-    //     return (
-    //         <div>
-    //             <Segment padded className={'search'}>
-    //                 <Header textAlign={'center'} as='h3'> Plan your Trip and Compensate your Emission </Header>
-    //                 <Grid>
-    //                     <Grid.Row>
-    //                         <Grid.Column width={12}>
-    //                             <Form>
-    //                             <Departure departure={this.handleScriptLoad()}/>
-    //                             <Arrival/>
-    //                             </Form>
-    //                         </Grid.Column>
-    //                         <Grid.Column verticalAlign={'middle'} textAlign={'center'} width={4}>
-    //                             <Button onClick={data_export = postCords} positive> Submit </Button>
-    //                         </Grid.Column>
-    //                     </Grid.Row>
-    //                 </Grid>
-    //             </Segment>
-    //         </div>
-    //     );
-    // }
 }
--- src/js/components/search/departure.js
+++ src/js/components/search/departure.js
@@ -1,45 +1,13 @@
 import React, {Component} from 'react';
-import {Form} from "semantic-ui-react";
-const form_style = {
-    margin: '0 auto',
-    maxWidth: 800,
-};
+import Script from 'react-load-script';
+import {Segment, Grid, Form, Button} from 'semantic-ui-react'
+import Header from "semantic-ui-react/dist/commonjs/elements/Header";
 
-export default class Departure extends Component{
-    render() {
-        return (
-            <Form.Input id={'departure'}
-                        placeholder={'Departure'}
-                        style={form_style}
-                        onChange={this.props.action}
-                        value={this.props.value}
-            />
-        )
-    }
-}
 
-    //
-    // handleScriptLoad() {
-    //     this.departure = new google.maps.places.Autocomplete(
-    //         document.getElementById('departure'));
-    //     this.departure.setFields(this.fields);
-    //     this.departure.addListener('place_changed', this.handleDeparture);
-    //
-    // }
-    // fields = ['address_components', 'geometry', 'icon', 'name'];
+const Departure = () => (
+ <div>
 
-    //
-    // handleDeparture = () => {
-    //     let place = this.departure.getPlace();
-    //     if (!place.geometry) {
-    //         // User entered the name of a Place that was not suggested and
-    //         // pressed the Enter key, or the Place Details request failed.
-    //         console.log("No details available for input: '" + place.name + "'");
-    //         return;
-    //     }
-    //
-    //     this.lat = place.geometry.location.lat();
-    //     this.lng = place.geometry.location.lng();
-    //
-    // };
+ </div>
+);
 
+export default Departure
