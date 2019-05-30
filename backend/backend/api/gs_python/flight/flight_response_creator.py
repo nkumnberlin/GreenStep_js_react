f70089c675998aeac13b202a4b5eb46fe73818b4
Tristan W <s0564076@HTW-Berlin.de>
2019-05-30 16:15:57 +0200

    Anpassung JSON-Aufbau der Flight-Response

--- backend/backend/api/gs_python/flight/flight_response_creator.py
+++ backend/backend/api/gs_python/flight/flight_response_creator.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from flight.flight_info import flight_data
 class flight_response:
+    emission_flight = 0.18
     def create_response(self, f_data):
 
                     # (self, arrival_transit_json, departure_transit_json,
@@ -14,11 +15,16 @@ class flight_response:
     def create_step_by_step_route_json(self, f_data):
         json = []
         json.append(f_data.departure_transit_json)
-        json.append([{"travel_mode": "FLYING",
+        json.append({
+            "flying":{
+               "dist":f_data.flight_dist,
+               "time":f_data.flight_time,
+               "emission":f_data.flight_dist / 1000 * self.emission_flight,
+               "steps":[{"travel_mode": "FLYING",
                       "Values": {
                           "duration": f_data.flight_time,
                           "distance": f_data.flight_dist,
                           "start_location": f_data.origin_airport["iata"],
-                          "end_location": f_data.dest_airport["iata"]}}])
+                          "end_location": f_data.dest_airport["iata"]}}]}})
         json.append(f_data.arrival_transit_json)
         return json
