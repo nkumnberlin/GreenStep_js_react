06b1be71691ba9abb24cf13cfb19cf6a9f0ab709
Tristan W <s0564076@HTW-Berlin.de>
2019-06-04 12:01:52 +0200

    Added distcalc_coords

--- backend/backend/api/gs_python/distcalc/calc_geographic_points.py
+++ backend/backend/api/gs_python/distcalc/calc_geographic_points.py
@@ -5,13 +5,23 @@ class distcalc:
       return degrees * math.pi / 180
 
     #Flugstreckenberechnung - mithilfe von math Funktionen
-    def distanceInKmBetweenEarthCoordinates(self, lat1, lon1, lat2, lon2):
+    def distanceInKmBetweenEarthCoordinates(self, lat1, lng1, lat2, lng2):
         earthRadiusKm = 6371
         dLat = self.degreesToRadians(lat2 - lat1)
-        dLon = self.degreesToRadians(lon2 - lon1)
+        dLon = self.degreesToRadians(lng2 - lng1)
         lat1 = self.degreesToRadians(lat1)
         lat2 = self.degreesToRadians(lat2)
         a = math.sin(dLat / 2) * math.sin(dLat / 2) + math.sin(dLon / 2) * math.sin(dLon / 2) * math.cos(lat1) * \
             math.cos(lat2)
         c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
         return earthRadiusKm * c * 1000
+
+class distcalc_coords:
+    def __init__(self, origin_lng, origin_lat, dest_lng, dest_lat):
+        self.lat1 = origin_lat
+        self.lng1 = origin_lng
+        self.lat2 = dest_lat
+        self.lng2 = dest_lng
+        
+    def __get__(self):
+        return distcalc().distanceInKmBetweenEarthCoordinates(self.lat1, self.lng1, self.lat2, self.lng2)
