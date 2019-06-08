cd403f7d3a89b3b1b3f7274733293f64da335a60
Kaan Özgiray <kaanoezgiray29@gmail.com>
2019-06-08 13:08:26 +0200

    Implemented MyFootprint

---
+++ src/js/components/compensation/MyFootprint.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import 'semantic-ui-css/semantic.min.css';
+
+
+class MyFootprint extends Component {
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+
+	}
+}
