2c0a06422f4f868c25d37d946eb4a4ad5603ec69
Kaan Özgiray <kaanoezgiray29@gmail.com>
2019-06-16 19:29:30 +0200

    Implemented GridComparison further

--- src/js/components/compensation/ComparisonCalculator.js
+++ src/js/components/compensation/ComparisonCalculator.js
@@ -4,10 +4,16 @@
 
 */
 	const amountCO2 = 27.5; // Berlin - Köln hin und zurück mit dem Zug (ICE)
+	const allowedCO2 = 1200;
 
 	export function equalsTrain(co2) {
 		let amountRides = Math.round(co2/amountCO2);
 		return amountRides;
+	}
+
+	export function allowedPercentage(co2) {
+		let percentage = Math.round(co2 / allowedCO2*100) / 100 * 100;
+		return percentage;
 	}
 
 
--- src/js/components/compensation/ComparisonGrid.jsx
+++ src/js/components/compensation/ComparisonGrid.jsx
@@ -1,23 +1,27 @@
 import React, {Fragment} from 'react';
 import { Grid, Image, Segment, Header} from 'semantic-ui-react';
 import {adjustEmissionValues} from "../data_handler/Converter.jsx";
+import * as utils from './ComparisonCalculator.js';
 
 export const GridVerticallyDivided = (prop) => {
+    let emission = Math.round(Number.parseFloat(prop) * 100) / 100;
+    let amountRides = utils.equalsTrain(emission);
+    let perc_Allowed = utils.allowedPercentage(emission);
     return(
       <Fragment>
         <Segment>
         <Header as={"h2"} id="vergleichHeader" textAlign="center">Zum Vergleich</Header>
-        <Header as={"h4"} id="didYouKnow" textAlign="center">Wussten Sie schon? {prop} kg CO2 entsprechen:</Header>
+        <Header as={"h4"} id="didYouKnow" textAlign="center">Wussten Sie schon? <span className="boldText">{emission}</span>kg CO2 entsprechen:</Header>
           <Grid divided='vertically'>
             <Grid.Row columns={3}>
               <Grid.Column>
                 <p>
-                  x einfachen Bahnfahrten Berlin - Köln.
+                  <span className="boldText">{amountRides}</span> einfachen Bahnfahrten Berlin - Köln.
                 </p>
               </Grid.Column>
               <Grid.Column>
                 <p>
-                  x % des durchschnittlichen CO2 Ausstoßes eines Bundesbürgers pro Jahr.
+                  <span className="boldText">{perc_Allowed}</span>% des offiziell erlaubten CO2 Ausstoßes eines Bundesbürgers pro Jahr.
                 </p>
               </Grid.Column>
               <Grid.Column>
