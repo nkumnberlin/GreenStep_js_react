7100ddeb983dcb03fa3def31b849acaae0ba51e9
Kaan Özgiray <kaanoezgiray29@gmail.com>
2019-06-19 00:50:30 +0200

    Added Atmosfair to Compensation Possibility

--- src/js/components/compensation/CompensationList.jsx
+++ src/js/components/compensation/CompensationList.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment} from 'react'
-import { List, Segment, Header } from 'semantic-ui-react'
+import { List, Segment, Header, Icon } from 'semantic-ui-react'
 
 export const ListExampleHeader = () => (
   <Fragment>
@@ -7,19 +7,19 @@ export const ListExampleHeader = () => (
       <Header as={"h3"} textAlign="center">Possibilites for Compensation:</Header>
       <List>
         <List.Item>
-          <List.Header>New York City</List.Header>
-          <a href="https://www.google.com/" target="_blank">A lovely city</a>
+          <List.Header as="h2"><Icon name="external alternate" size="small"/><a href="https://www.google.com/" target="_blank">Atmosfair</a></List.Header>
+          <List.Description className="compDes">Go climate conscious <Icon name="angle right" /> Offset flight <Icon name="angle right" /> Offset cruise <Icon name="angle right" /> Offset desired CO2 value <Icon name="angle right" /> Donate directly.</List.Description>
         </List.Item>
         <List.Item>
-          <List.Header>Chicago</List.Header>
+          <List.Header as="h3">Chicago</List.Header>
           Also quite a lovely city
         </List.Item>
         <List.Item>
-          <List.Header>Los Angeles</List.Header>
+          <List.Header as="h3">Los Angeles</List.Header>
           Sometimes can be a lovely city
         </List.Item>
         <List.Item>
-          <List.Header>San Francisco</List.Header>
+          <List.Header as="h3">San Francisco</List.Header>
           What a lovely city
         </List.Item>
       </List>
